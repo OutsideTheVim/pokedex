d7c5115592f72d367b5594931239d801121aca87
Nathan Yassin <codeventior@gmail.com>
2023-11-06 01:15:40 +0100

    Question dialog added to explain meaning of pokemon ID

--- app/src/main/java/com/nathanyd/pokedex/ui/screens/PokeScreen.kt
+++ app/src/main/java/com/nathanyd/pokedex/ui/screens/PokeScreen.kt
@@ -2,6 +2,7 @@ package com.nathanyd.pokedex.ui.screens
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -18,9 +19,14 @@ import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
 import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -34,6 +40,7 @@ import androidx.compose.ui.unit.sp
 import com.nathanyd.pokedex.R
 import com.nathanyd.pokedex.ui.screens.shared.PokeImage
 import com.nathanyd.pokedex.ui.screens.shared.PokeType
+import com.nathanyd.pokedex.ui.screens.shared.QuestionBox
 
 //Screen for Pokemon info page
 @Composable
@@ -54,6 +61,17 @@ fun DefaultPokeScreen(modifier: Modifier = Modifier) {
 //Pokemon information card
 @Composable
 fun PokeStatus() {
+
+    var questionBox: Boolean by remember {
+        mutableStateOf(false)
+    }
+
+    if(questionBox) QuestionBox(
+        clicked = { questionBox = false },
+        title = "What does #1 mean?",
+        description = stringResource(id = R.string.number_question_desc, "#1")
+    )
+
     Column {
         Row(modifier = Modifier.padding(16.dp), verticalAlignment = Alignment.CenterVertically) {
             Text(
@@ -76,12 +94,23 @@ fun PokeStatus() {
         StatsBar("Special Attack", 0.65f)
         StatsBar("Special Defense", 0.65f)
         Spacer(modifier = Modifier.weight(1f))
-        Text(
-            text = "#1",
-            modifier = Modifier.padding(bottom = 48.dp),
-            fontSize = 48.sp,
-            fontWeight = FontWeight.Bold
-        )
+        Row {
+            Text(
+                text = "#1",
+                modifier = Modifier.padding(bottom = 48.dp),
+                fontSize = 48.sp,
+                fontWeight = FontWeight.Bold
+            )
+            Icon(
+                painter = painterResource(id = R.drawable.help_icon),
+                contentDescription = null,
+                modifier = Modifier
+                    .size(24.dp)
+                    .clickable {
+                        questionBox = true
+                    }
+            )
+        }
     }
 }
 
